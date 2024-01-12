22d873acd465e436d982904b27a032567a061301
YashDxr <yashdxr@gmail.com>
2024-01-12 10:17:26 +0530

    removed unnecessary code

--- src/modules/FormOutput.jsx
+++ src/modules/FormOutput.jsx
@@ -1,39 +1,4 @@
 import { useEffect, useState } from "react";
-import Input from "../components/Input";
-
-// export default function FormOutput({ jsonData }) {
-//   const [formData, setFormData] = useState("");
-//   useEffect(() => {
-//     console.log(jsonData);
-//     setFormData(jsonData);
-//   }, [jsonData]);
-
-//   const jsonArray = JSON.parse(formData);
-
-//   return (
-//     <div>
-//       {jsonArray.length > 0 &&
-//         jsonArray.map((e) => {
-//           console.log("Data", e);
-//         })}
-//     </div>
-//   );
-// }
-
-// {
-//     "sort": 1,
-//     "label": "Pizza Name",
-//     "description": "",
-//     "validate": {
-//       "required": true,
-//       "immutable": false
-//     },
-//     "jsonKey": "name",
-//     "uiType": "Input",
-//     "icon": "",
-//     "level": 0,
-//     "placeholder": ""
-//   },
 
 export default function FormOutput({ jsonData }) {
   const [formData, setFormData] = useState("");
